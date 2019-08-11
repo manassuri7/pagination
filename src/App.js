b1f38888933c8c69d466b91119f84a57f868e186
manassuri7 <49301320+manassuri7@users.noreply.github.com>
2019-08-11 13:58:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import Posts from './components/Posts';
+import Pagination from './components/Pagination';
+
+import './App.css';
+
+function App() {
+  const [posts,setPosts]=useState([]);
+  const [loading,setLoading]=useState(false);
+  const [currentPage,setCurrentPage]=useState(1);
+  const [postsPerPage]=useState(10);
+
+  useEffect(()=>{
+    const fetchPosts=async()=>{
+      setLoading(true);
+      const res=await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+    fetchPosts();
+  },[]);//[] is put so tht this useEffect runs only once.
+  console.log(posts);
+
+//get current posts
+const indexOfLastPost=currentPage*postsPerPage;
+const indexOfFirstPost=indexOfLastPost-postsPerPage;
+const currentPosts=posts.slice(indexOfFirstPost,indexOfLastPost);
+
+//Change Page
+const paginate=(pageNumber)=>setCurrentPage(pageNumber);
+
+
+  return (
+    <div className="container mt-5">
+    <h1 className="text-primary mb-3">My Blog</h1>
+    <Posts posts={currentPosts} loading={loading} />
+    <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate} />
+    </div>
+  );
+}
+
+export default App;
